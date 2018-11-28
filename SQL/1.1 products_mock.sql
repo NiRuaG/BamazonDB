f6421569e8f76532c9498a7d62513507aa61711b
NiRuaG <gaurin1107@gmail.com>
2018-11-27 22:53:00 -0600

    dept values

--- SQL/1.1 products_mock.sql
+++ SQL/1.1 products_mock.sql
@@ -3,18 +3,18 @@ USE `bamazon`;
 TRUNCATE TABLE `products`;
 
 INSERT INTO `products` 
-  (`item_id`, `product_name`, `price`, `stock_quantity`)
+  (`item_id`, `product_name`, `department_name`, `price`, `stock_quantity`)
 VALUES 
-  (   19, "vel pede"               ,   9.23, 16),
-  (   89, "donec quis"             , 132.30, 45),
-  (  810, "nibh"                   ,   2.23,  0),
-  ( 1641, "metus arcu"             , 101.71,  3),
-  ( 3218, "amet fermentum"         ,   0.96, 73),
-  ( 3309, "nec euismod scelerisque",   2.91,  1),
-  ( 4918, "ipsum integer"          ,   8.08, 57),
-  ( 6588, "sem duis aliquam"       ,   4.15, 42),
-  ( 7029, "turpis elementum"       ,  12.25, 16),
-  (21356, "luctus et"              ,  53.34, 73)
+  (   19, "vel pede"               , "Outdoors",   9.23, 16),
+  (   89, "donec quis"             , "Outdoors", 132.30, 45),
+  (  810, "nibh"                   , "Toys"    ,   2.23,  0),
+  ( 1641, "metus arcu"             , "Beauty"  , 101.71,  3),
+  ( 3218, "amet fermentum"         , "Tools"   ,   0.96, 73),
+  ( 3309, "nec euismod scelerisque", "Clothing",   2.91,  1),
+  ( 4918, "ipsum integer"          , "Toys"    ,   8.08, 57),
+  ( 6588, "sem duis aliquam"       , "Music"   ,   4.15, 42),
+  ( 7029, "turpis elementum"       , "Health"  ,  12.25, 16),
+  (21356, "luctus et"              , "Music"   ,  53.34, 73)
 ;
 
 ALTER TABLE `products` AUTO_INCREMENT=33182;
